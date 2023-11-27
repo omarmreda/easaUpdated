9eefb90445b44b4568d4cc8b2b3cdfe3cb85e1c7
omarmreda95 <omarmreda95@gmail.com>
2023-11-27 14:09:39 +0200

    social media updates

--- js/custom.js
+++ js/custom.js
@@ -791,6 +791,9 @@ $(window).on("load", function (e) {
 	 setTimeout(function(){
 		jQuery('#loading-area').remove();
 	}, 0); 
+	const numbers = document.getElementsByClassName("numberDecimal")
+
+numbers.innerHTML.value = parseFloat(this.value).toFixed(2)
 });
 /*  Window Load END */
 
@@ -800,3 +803,22 @@ jQuery(window).on('resize',function () {
 	Constructzilla.resize();
 });
 /*  Window Resize END */
+
+    // mini jQuery plugin that formats to two decimal places
+    (function($) {
+        $.fn.currencyFormat = function() {
+            this.each( function( i ) {
+                $(this).change( function( e ){
+                    if( isNaN( parseFloat( this.value ) ) ) return;
+                    this.value = parseFloat(this.value).toFixed(2);
+                });
+            });
+            return this; //for chaining
+        }
+    })( jQuery );
+
+    // apply the currencyFormat behaviour to elements with 'currency' as their class
+    $( function() {
+        $('.currency').currencyFormat();
+    });
+
